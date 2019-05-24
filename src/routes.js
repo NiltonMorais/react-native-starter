dee5de4656fbc7f93fa8bf08c7b6959e8b6d38d2
Nilton Morais <niltonmorais.code@gmail.com>
2019-05-24 17:01:03 -0300

    estilizando header e status bar

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from 'react-navigation';
+
+import Main from './pages/main';
+
+export default createStackNavigator({
+    Main
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#DA552F'
+        },
+        headerTintColor: "#FFF"
+    }
+});
